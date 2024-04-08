36db2a824d2c927442a488e35aa439a9e2566ca7
Benjamin8508 <163232174+Benjamin8508@users.noreply.github.com>
2024-04-08 22:55:37 +0900

    Add files via upload

---
+++ ddrf.py
@@ -0,0 +1,13 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from numpy.linalg import matrix_power
+import math
+import pandas as pd
+from datetime import datetime
+from scipy.signal import find_peaks
+from scipy.linalg import expm, sinm, cosm
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import qutip as qt
+from scipy.sparse import csr_matrix
+import torch
